01e7f25a478018a22e2e7ef4b37b62caf56a7a04
minseok0616 <minseok6815@naver.com>
2021-09-28 21:51:33 +0900

    c++

---
+++ 조건문, 반복문, 네임스페이스/for.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i;
+
+	for (i = 1;i <= 5;i++)
+		cout << "i=" << i << endl;
+
+	cout << "반복문 for 이후 i의 값: " << i << endl;
+
+	return 0;
+}
---
+++ 조건문, 반복문, 네임스페이스/while.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i;
+
+	i = 1;
+	while (i <= 5)
+	{
+		cout << "i=" << i << endl;
+		i++;
+
+	}
+	cout << "반복문 while 이후의 i의 값" << i << endl;
+
+	return 0;
+
+}
+
